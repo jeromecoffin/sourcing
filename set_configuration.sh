840124a37d91bba338fa0ede7f5fd2b67f33f070
jeromecoffin <jerome.devops@gmail.com>
2024-08-27 14:15:36 +0700

    starting nginx

---
+++ set_configuration.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+
+docker exec -u www-data nextcloud php occ --no-warnings config:system:get trusted_domains >> trusted_domain.tmp
+
+if ! grep -q "nginx-server" trusted_domain.tmp; then
+    TRUSTED_INDEX=$(cat trusted_domain.tmp | wc -l);
+    docker exec -u www-data nextcloud php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="nginx-server"
+fi
+
+rm trusted_domain.tmp
+
+docker exec -u www-data nextcloud php occ --no-warnings app:install onlyoffice
+
+docker exec -u www-data nextcloud php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
+docker exec -u www-data nextcloud php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
+docker exec -u www-data nextcloud php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
+docker exec -u www-data nextcloud php occ --no-warnings config:system:set onlyoffice jwt_secret --value="secret"
