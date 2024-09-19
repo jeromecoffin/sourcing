6a3812a6539618299cef79e44214136037cb0026
jeromecoffin <jerome.devops@gmail.com>
2024-09-19 16:20:05 +0700

    new image tag and JWT secret

--- set_configuration.sh
+++ set_configuration.sh
@@ -31,7 +31,11 @@ docker exec -u www-data storage-nextcloud php occ --no-warnings app:install only
 docker exec -u www-data storage-nextcloud php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
 docker exec -u www-data storage-nextcloud php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
 docker exec -u www-data storage-nextcloud php occ --no-warnings config:system:set onlyoffice StorageUrl --value="https://nginx-server:8443/" 
-docker exec -u www-data storage-nextcloud php occ --no-warnings config:system:set onlyoffice jwt_secret --value="secret" # TODO: set secret. Voir docker-compose.yml
-# TODO: .services.CoAuthoring.requestDefaults.rejectUnauthorized = false
-# docker cp default.json onlyoffice-document-server:/etc/onlyoffice/documentserver/default.json
-# docker exec -it -d onlyoffice-document-server ./app/ds/run-document-server.sh
+docker exec -u www-data storage-nextcloud php occ --no-warnings config:system:set onlyoffice jwt_secret --value=$JWT_SECRET # TODO: set secret. Voir docker-compose.yml
+
+# Set .services.CoAuthoring.requestDefaults.rejectUnauthorized = false
+docker cp default.json onlyoffice-document-server:/etc/onlyoffice/documentserver/default.json
+docker exec -it -d onlyoffice-document-server ./app/ds/run-document-server.sh
+
+# Attention si image nextcloud plus récente s'installe : {"error":"Strict Cookie has not been found in request"}
+# Il faut downgraded et donc supprimer le volume sinon : Can't start Nextcloud because the version of the data (21.0.0.18) is higher than the docker image version (19.0.9.1) and downgrading is not supported. Are you sure you have pulled the newest image version?
